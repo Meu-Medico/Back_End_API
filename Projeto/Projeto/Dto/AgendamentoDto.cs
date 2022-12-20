5701099c02cdf57d29fbe10bc9149600bd1e7bf0
Isabel Pereira de Souza <contatobelsouza@gmail.com>
2022-12-20 15:07:05 -0300

    Controller and Dto

---
+++ Projeto/Projeto/Dto/AgendamentoDto.cs
@@ -0,0 +1,28 @@
+﻿using Projeto.Entidade;
+
+namespace Projeto.Dto
+{
+    public class AgendamentoDto
+    {
+        public int IdAgendamento { get; set; }
+        public int IdHospital { get; set; }
+
+        public int IdEspecialidade { get; set; }
+
+        public int IdProfissional { get; set; }
+
+        public DateTime DataHoraAgendamento { get; set; }
+
+        public int IdBeneficiario { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public virtual Beneficiario IdBeneficiarioNavigation { get; set; } = null!;
+
+        public virtual Especialidade IdEspecialidadeNavigation { get; set; } = null!;
+
+        public virtual Hospital IdHospitalNavigation { get; set; } = null!;
+
+        public virtual Profissional IdProfissionalNavigation { get; set; } = null!;
+    }
+}
